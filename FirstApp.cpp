babf20bfcbc9b6fd109f49924fcd176637b817b0
ElopingDev <elopingbuisness@gmail.com>
2022-06-30 12:53:58 +0200

    Add project files.

---
+++ FirstApp.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>	
+using namespace std;
+
+int main()
+{
+    string femboy;
+    start:
+    cout << "Welcome to Femboy Hooters !\n"; // I am extremely racist
+    cout << "Are you a femboy?\n";
+    cin >> femboy;
+    if (femboy == "Yes")
+    {
+        cout << "Let's have anal sex >.< M-maybe you could get me b-boypreggers..\n";
+    }
+    else if (femboy == "No") {
+        cout << "You're not allowed here uwu.";
+    }
+    else
+        cout << "Please answer by Yes or No\n";
+    goto start;
+    system("pause>0");
+}
+
+
